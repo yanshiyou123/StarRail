f296aec1e73a2ec6f9c2f91e17954aaa5f2f7e00
yanshiyou123 <ysy200087@gmail.com>
2024-01-07 12:47:37 -0600

    v1.1

--- frontend-StarRail/pages/starRail/main.js
+++ frontend-StarRail/pages/starRail/main.js
@@ -15,13 +15,13 @@ export default function Main() {
       <div className="container">
         <Row>
           <Col md={4}>
-            <Card title="软媒" text="666" image="/images/RuanMei.jpg" />
+            <Card title="阮梅" text="666" image="/images/RuanMei.jpg" />
           </Col>
           <Col md={4}>
-            <Card title="软媒" text="666" image="/images/RuanMei.jpg" />
+            <Card title="银枝" text="666" image="/images/Argenti.jpg" />
           </Col>
           <Col md={4}>
-            <Card title="软媒" text="666" image="/images/RuanMei.jpg" />
+            <Card title="托帕&账账" text="666" image="/images/Topaz.jpg" />
           </Col>
         </Row>
       </div>
